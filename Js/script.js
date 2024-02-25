3cb62eafd72bb22aa39fc0ec1d45bc4f0cd62931
Lorenzo Pina <al214776@alumnos.uacj.mx>
2024-02-25 01:47:53 -0700

    New Commit

--- Js/script.js
+++ Js/script.js
@@ -1,34 +1,35 @@
-var swiper = new Swiper (".mySwiper-1", {
-    slidesPerView:1,
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView: 1,
     spaceBetween: 30,
-    loop:true,
+    loop: true,
     pagination: {
-        el:".swiper-pagination",
+        el: ".swiper-pagination",
         clickable: true,
     },
     navigation: {
-        nextEl:".swiper-button-next",
-        prevEl:".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     }
 });
-var swiper = new Swiper (".mySwiper-2", {
-    slidesPerView:3,
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView: 3,
     spaceBetween: 20,
-    loop:true,
-    loopFillGroupWithBlank:true,
+    loop: true,
+    loopFillGroupWithBlank: true,
     navigation: {
-        nextEl:".swiper-button-next",
-        prevEl:".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    breakpoints : {
+    breakpoints: {
         0: {
-            slidesPerView:1,
+            slidesPerView: 1,
         },
         520: {
-            slidesPerView:2,
+            slidesPerView: 2,
         },
         950: {
-            slidesPerView:3,
+            slidesPerView: 3,
         }
 
     }
@@ -36,9 +37,9 @@ var swiper = new Swiper (".mySwiper-2", {
 
 let tabInputs = document.querySelectorAll(".tabInput");
 
-tabInputs.forEach(function(input){
+tabInputs.forEach(function (input) {
 
-    input.addEventListener('change', function(){
+    input.addEventListener('change', function () {
         let id = input.ariaValueMax;
         let thisSwiper = document.getElementById('swiper' + id);
         thisSwiper.swiper.update();
