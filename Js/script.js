9665096d754a992ec97ba61ea1b74d2371136b13
Lorenzo Pina <al214776@alumnos.uacj.mx>
2024-02-25 01:15:54 -0700

    Four commit

--- Js/script.js
+++ Js/script.js
@@ -1,46 +1,48 @@
-    var swiper = new swiper (".mySwiper-1", {
-        slidesPerView:1,
-        spaceBetween: 30,
-        loop:true,
-        pagination: {
-            el:".swiper-pagination",
-            clickable: true,
+var swiper = new Swiper (".mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+});
+var swiper = new Swiper (".mySwiper-2", {
+    slidesPerView:3,
+    spaceBetween: 20,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    },
+    breakpoints : {
+        0: {
+            slidesPerView:1,
         },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            nextEl: ".swiper-button-prev",
-        }
-    })    
-    var swiper = new swiper (".mySwiper-2", {
-        slidesPerView:3,
-        spaceBetween: 20,
-        loop:true,
-        loopFillGroupWithBlank:true,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            nextEl: ".swiper-button-prev",
+        520: {
+            slidesPerView:2,
         },
-        breakpoint: {
-            0:{
-                slidesPerView: 1,
-            },        
-            520:{
-                slidesPerView: 2,
-            },            
-            950:{
-                slidesPerView: 3,
-            }
+        950: {
+            slidesPerView:3,
         }
-    })
 
-let tabInputs = document.querySelectorAll (".tabInput");
+    }
+});
+
+let tabInputs = document.querySelectorAll(".tabInput");
 
 tabInputs.forEach(function(input){
 
-    input.addEventListener('change', function() {
+    input.addEventListener('change', function(){
         let id = input.ariaValueMax;
         let thisSwiper = document.getElementById('swiper' + id);
         thisSwiper.swiper.update();
+
     })
 
 });
